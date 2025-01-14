310ac4c8f96715c902a91356233a83dc3954259f
ArchitGangshettiwar <architgangshettiwar@gmail.com>
2025-01-14 15:15:18 +0530

    Delete secret_project directory

--- secret_project/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("magicButton").addEventListener("click", function() {
-  const surpriseMessage = document.getElementById("surpriseMessage");
-  surpriseMessage.classList.toggle("hidden");
-});
